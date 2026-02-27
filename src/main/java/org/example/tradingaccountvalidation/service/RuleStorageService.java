4670c662f19db9e6bf6bbac2b5a8f736da05b146
Sabharish <sabharishhhpv2304@gmail.com>
2026-02-27 18:29:40 +0530

    log improved

--- src/main/java/org/example/tradingaccountvalidation/service/RuleStorageService.java
+++ src/main/java/org/example/tradingaccountvalidation/service/RuleStorageService.java
@@ -1,10 +1,12 @@
 package org.example.tradingaccountvalidation.service;
 
+import org.example.tradingaccountvalidation.controller.TradingAccountValidationController;
 import org.example.tradingaccountvalidation.repo.RuleStorageInterface;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.Objects;
@@ -13,23 +15,26 @@ import java.util.Objects;
 public class RuleStorageService implements RuleStorageInterface {
     @Value("${rules.folder}")
     private String rulesFolder;
+    private static final Logger log = LoggerFactory.getLogger(TradingAccountValidationController.class);
 
     private static final long MAX_SIZE = 5 * 1024 * 1024;
 
     @Override
     public void validate(MultipartFile file) {
         if (file == null || file.isEmpty()) {
-            log.info()
+            log.info("File is empty");
             throw new RuntimeException("File is empty");
         }
 
         if (file.getSize() > MAX_SIZE) {
+            log.info("File > 5 MB");
             throw new RuntimeException("File exceeds 5 MB");
         }
 
         String name = file.getOriginalFilename();
 
         if (name == null || !name.toLowerCase().endsWith(".xlsx")) {
+            log.info("Invalid file type, .xlsx files only allowed");
             throw new RuntimeException("Only .xlsx files allowed");
         }
     }
