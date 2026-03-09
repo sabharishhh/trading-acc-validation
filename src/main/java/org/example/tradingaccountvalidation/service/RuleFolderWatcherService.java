3cc423480075a873fb315c44b062a8c00eee8576
Sabharish <sabharishhhpv2304@gmail.com>
2026-03-09 13:43:00 +0530

    file naming consistency

--- src/main/java/org/example/tradingaccountvalidation/service/RuleFolderWatcherService.java
+++ src/main/java/org/example/tradingaccountvalidation/service/RuleFolderWatcherService.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import java.nio.file.*;
 import static java.nio.file.StandardWatchEventKinds.*;
 
